a1de8a913c492d82f9aa6c2bc39836ba9ffeba4e
Joaquin <joaquin@autotel.co>
2017-04-28 12:20:47 +0900

    message simulation of long format messages.

--- js/message.js
+++ js/message.js
@@ -1,46 +1,61 @@
 var Message=function(data){
+  //I made this class to simulate message communication in a hardware situation
+  //it is utterly useless in the context of javascript
   var tMessage=this;
+  var headerLocation=0;
+
+
   if(data.length==0){
     console.log("data is invalid");
     return false;
   }
+  if(data.length>3){
+    //if data longer than three, we use a long message configuration
+    headerLocation=1;
+    data.unshift(0xF0);
+    data.push(0x0F);
+  }
   this.data=data;
   this.header=function(){
-    return data[0];
+    return data[headerLocation];
   }
   this.payload=function(){
-    return data.slice(1,data.length);
+    return data.slice(headerLocation+1,data.length);
   }
   this.headerFunction={
     set:function(to){
       //mask data removing current headerAddress
-      data[0]&=0x0F;
+      data[headerLocation]&=0x0F;
       //add new headerFunction
-      data[0]|=(to<<4)&0xF0;
+      data[headerLocation]|=(to<<4)&0xF0;
       if(to>15)
-      console.warn("function header overflow! "+to+" became "+data[0]);
+      console.warn("function header overflow! "+to+" became "+data[headerLocation]);
     },
     get:function(to){
-      return (data[0]&0xF0)>>4;
+      return (data[headerLocation]&0xF0)>>4;
     },
   }
   this.headerAddress={
     set:function(to){
       //mask data removing current headerFunction
-      data[0]&=0xF0;
+      data[headerLocation]&=0xF0;
       //add new headerAddress
-      data[0]|=to&0x0F;
+      data[headerLocation]|=to&0x0F;
       if(to>15)
-      console.warn("function header overflow! "+to+" became "+data[0]);
+      console.warn("function header overflow! "+to+" became "+data[headerLocation]);
     },
     get:function(to){
-      return data[0]&0xf;
+      return data[headerLocation]&0xf;
     },
   }
   this.log=function(){
     var hexString="[";
-    for(var a of data){
-      hexString+="0x"+a.toString(16)+",";
+    for(var a in data){
+      hexString+="0x"+data[a].toString(16);
+      if(headerLocation==a)
+      hexString+="(h)"
+      if(a!=data.length-1)
+      hexString+=",";
     }
     hexString+="]";
     console.log(hexString,data);
